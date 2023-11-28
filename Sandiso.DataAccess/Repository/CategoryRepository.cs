02db8845c723a4745443c502a4bc19dcb73fd89b
Sandiso2080 <91184967+Sandiso2080@users.noreply.github.com>
2023-11-28 10:19:06 +0200

    Add project files.

---
+++ Sandiso.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Sandiso.DataAccess.Data;
+using Sandiso.DataAccess.Repository.IRepository;
+using Sandiso.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandiso.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) :base (db) 
+        { 
+            _db = db;
+        }
+        public void save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
